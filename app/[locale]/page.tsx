db8d7bbb76d3ba2513e01d22757ec1d252f02ee0
tkgshn <takagishunsuke1129@gmail.com>
2025-04-15 18:05:15 +0900

    Initial commit

--- app/[locale]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslations } from 'next-intl'
-
-export default function HomePage() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl font-bold">Plurality Tokyo</h1>
-        </main>
-    )
-}
